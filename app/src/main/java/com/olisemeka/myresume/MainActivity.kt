acf3d0491a67aa046a63f2dfe7b8c96da8eb0234
Nwaeme Olisemeka <35772751+nwaeme-olise@users.noreply.github.com>
2022-11-04 03:06:16 +0100

    Implemented Navigation Drawer and connected with MainActivity.kt

--- app/src/main/java/com/olisemeka/myresume/MainActivity.kt
+++ app/src/main/java/com/olisemeka/myresume/MainActivity.kt
@@ -2,10 +2,25 @@ package com.olisemeka.myresume
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.olisemeka.myresume.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val config = AppBarConfiguration(navController.graph, binding.drawerLayout)
+        binding.toolbar.setupWithNavController(navController, config)
+        binding.navView.setupWithNavController(navController)
     }
 }
