a4a3aa0d86da08c7e22fac1cb400e018bb02cc23
Nwaeme Olisemeka <35772751+nwaeme-olise@users.noreply.github.com>
2022-11-04 16:39:56 +0100

    Implemented ContactMeFragment.kt

--- app/src/main/java/com/olisemeka/myresume/ContactMeFragment.kt
+++ app/src/main/java/com/olisemeka/myresume/ContactMeFragment.kt
@@ -1,59 +1,35 @@
 package com.olisemeka.myresume
 
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.olisemeka.myresume.databinding.FragmentContactMeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ContactMeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ContactMeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+   private var _binding: FragmentContactMeBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact_me, container, false)
+        _binding = FragmentContactMeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ContactMeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ContactMeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupHyperlinks()
+    }
+
+    private fun setupHyperlinks() {
+        binding.tvGithub.movementMethod = LinkMovementMethod.getInstance()
+        binding.tvLinkedIn.movementMethod = LinkMovementMethod.getInstance()
     }
 }
